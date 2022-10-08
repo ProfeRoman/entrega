96a0a4fda90bffebb5a288b63806d6c3d9cc16c7
ProfeRoman <rscollo@hotmail.com>
2022-10-08 13:20:10 -0300

    dale con el CORS

--- rsf/src/main/java/com/portfolio/rsf/Controller/CHys.java
+++ rsf/src/main/java/com/portfolio/rsf/Controller/CHys.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController //le decimos que es un controller
 //@CrossOrigin(origins = "http://localhost:4200") //le decimos el origen
-@CrossOrigin(origins = "https://frontendscollouno.web.app")
+@CrossOrigin(origins = "https://frontendscollouno.web.app/portfolio")
 @RequestMapping("/skill")
 public class CHys {
 
