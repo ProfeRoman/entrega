5e4a0e2ae2d8b33dd02a998d6c38613ae5aa1235
ProfeRoman <rscollo@hotmail.com>
2022-10-11 20:06:26 -0300

    make it better

--- rsf/src/main/java/com/portfolio/rsf/WebConfiguration.java
+++ rsf/src/main/java/com/portfolio/rsf/WebConfiguration.java
@@ -1,14 +1,22 @@
 package com.portfolio.rsf;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfiguration {
 
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
     }
 }
